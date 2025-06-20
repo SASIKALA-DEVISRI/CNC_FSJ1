85eff5b844bc8b27a4ad0fde12491634bd40cb6e
SASIKALA_MUNIYANDI <145658859+SASIKALA-DEVISRI@users.noreply.github.com>
2025-06-20 09:32:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// Smooth scroll effect for nav links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
+  
